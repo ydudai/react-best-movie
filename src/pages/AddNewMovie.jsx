05b47f198f05d9ecf0c17e6bb0825d78b86be26d
ydudai <ydudai@outlook.com>
2025-03-13 18:09:17 +0200

    Correct   AddNewMovie

--- src/pages/AddNewMovie.jsx
+++ src/pages/AddNewMovie.jsx
@@ -1,19 +1,7 @@
 import React, { useContext, useState } from 'react'
 
 export default function AddNewMovie() {
-  // return (
-  //   <div>
-  //     <div className="flex-none bg-gray-300 p-4 border-2 min-h-96">
-  //       <div className="flex items-center">
-  //         <h2 className="text-sm font-medium mr-2">Add New Movie!</h2>
-
-  //       </div>
-  //       <div className="flex-none">
-  //         <textarea value="" rows="3" cols="50"></textarea>
-  //       </div>
-  //    </div>
-  //   </div>
-  // )
+ 
   const [name, setName] = useState("");
   const [pictureUrl, setPictureUrl] = useState("");
   const [description, setDescription] = useState("");
